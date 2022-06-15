b700030f8e8b9661cdec552de4d2a2825bbe5538
Eric Nordelo <eric.nordelo39@gmail.com>
2022-06-15 18:25:23 +0200

    feat: add branching coverage to tests

--- test/crosschain/CrossChainEnabled.test.js
+++ test/crosschain/CrossChainEnabled.test.js
@@ -2,6 +2,7 @@ const { BridgeHelper } = require('../helpers/crosschain');
 const { expectRevertCustomError } = require('../helpers/customError');
 const { expectEvent } = require('@openzeppelin/test-helpers');
 const { utils } = require('ethers');
+const { web3 } = require('hardhat');
 
 function randomAddress () {
   return web3.utils.toChecksumAddress(web3.utils.randomHex(20));
@@ -47,27 +48,32 @@ contract('CrossChainEnabled', function () {
   });
 
   describe('Arbitrum-L1', function () {
+    const bridgeId = '0xf99ba2be00af9942e39d17830153f4e6000e00d50e0db314b50f3740e70a7015';
+    const bridgeParameters = {
+      bridgeId,
+      totalL2GasCosts: 0,
+      l2CallValue: 0,
+      maxSubmissionCost: 10,
+      excessFeeRefundAddress: randomAddress(),
+      callValueRefundAddress: randomAddress(),
+      maxGas: utils.parseEther('0.1'),
+      gasPriceBid: 5,
+    };
+
+    function encodeArbitrumL1Params (params) {
+      return utils.defaultAbiCoder.encode(
+        ['bytes32', 'uint256', 'uint256', 'uint256', 'address', 'address', 'uint256', 'uint256'],
+        [...Object.values(params)],
+      );
+    }
+
     beforeEach(async function () {
       this.bridge = await BridgeHelper.deploy('Arbitrum-L1');
       this.contract = await CrossChainEnabledArbitrumL1Mock.new(this.bridge.address);
     });
 
     it('should emit L1 to L2 retryable ticket id', async function () {
-      const bridgeParameters = {
-        bridgeId: '0xf99ba2be00af9942e39d17830153f4e6000e00d50e0db314b50f3740e70a7015',
-        totalL2GasCosts: 0,
-        l2CallValue: 0,
-        maxSubmissionCost: 10,
-        excessFeeRefundAddress: randomAddress(),
-        callValueRefundAddress: randomAddress(),
-        maxGas: utils.parseEther('0.1'),
-        gasPriceBid: 5,
-      };
-
-      const crossChainTxParameters = utils.defaultAbiCoder.encode(
-        ['bytes32', 'uint256', 'uint256', 'uint256', 'address', 'address', 'uint256', 'uint256'],
-        [...Object.values(bridgeParameters)],
-      );
+      const crossChainTxParameters = encodeArbitrumL1Params(bridgeParameters);
 
       expectEvent(
         await this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters),
@@ -75,25 +81,40 @@ contract('CrossChainEnabled', function () {
       );
     });
 
+    it('should restrict cross-chain calls with wrong bridge Id', async function () {
+      const wrongBridgeId = web3.utils.keccak256('wrong bridge id');
+
+      bridgeParameters.bridgeId = wrongBridgeId;
+
+      const crossChainTxParameters = encodeArbitrumL1Params(bridgeParameters);
+
+      await expectRevertCustomError(
+        this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters),
+        `InvalidTargetBridge("${wrongBridgeId}", "${bridgeId}")`,
+      );
+    });
+
     shouldBehaveLikeReceiver();
   });
 
   describe('Arbitrum-L2', function () {
+    const bridgeId = '0xcf0303bf7c331f43c2fd71966f5e588be6e9b5778b20d0816d972ad9d72b0550';
+    const bridgeParameters = {
+      bridgeId,
+      depositValue: 0,
+    };
+
+    function encodeArbitrumL2Params (params) {
+      return utils.defaultAbiCoder.encode(['bytes32', 'uint256'], [...Object.values(params)]);
+    }
+
     beforeEach(async function () {
       this.bridge = await BridgeHelper.deploy('Arbitrum-L2');
       this.contract = await CrossChainEnabledArbitrumL2Mock.new();
     });
 
     it('should emit L2 to L1 transaction id', async function () {
-      const bridgeParameters = {
-        bridgeId: '0xcf0303bf7c331f43c2fd71966f5e588be6e9b5778b20d0816d972ad9d72b0550',
-        depositValue: 0,
-      };
-
-      const crossChainTxParameters = utils.defaultAbiCoder.encode(
-        ['bytes32', 'uint256'],
-        [...Object.values(bridgeParameters)],
-      );
+      const crossChainTxParameters = encodeArbitrumL2Params(bridgeParameters);
 
       expectEvent(
         await this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters),
@@ -101,52 +122,94 @@ contract('CrossChainEnabled', function () {
       );
     });
 
-    shouldBehaveLikeReceiver();
-  });
+    it('should restrict cross-chain calls with wrong bridge Id', async function () {
+      const wrongBridgeId = web3.utils.keccak256('wrong bridge id');
 
-  describe('Optimism-L1', function () {
-    beforeEach(async function () {
-      this.bridge = await BridgeHelper.deploy('Optimism');
-      this.contract = await CrossChainEnabledOptimismL1Mock.new(this.bridge.address);
-    });
+      bridgeParameters.bridgeId = wrongBridgeId;
 
-    it('should send cross-chain message successfuly', async function () {
-      const bridgeParameters = {
-        bridgeId: '0x8a69005a3baed81c73049b861e928740f14876213fc1cafd636c7aa14c2576b1',
-        gasLimit: utils.parseEther('0.000000000001'),
-      };
+      const crossChainTxParameters = encodeArbitrumL2Params(bridgeParameters);
 
-      const crossChainTxParameters = utils.defaultAbiCoder.encode(
-        ['bytes32', 'uint32'],
-        [...Object.values(bridgeParameters)],
+      await expectRevertCustomError(
+        this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters),
+        `InvalidTargetBridge("${wrongBridgeId}", "${bridgeId}")`,
       );
-
-      await this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters);
     });
 
     shouldBehaveLikeReceiver();
   });
 
-  describe('Optimism-L2', function () {
-    beforeEach(async function () {
-      this.bridge = await BridgeHelper.deploy('Optimism');
-      this.contract = await CrossChainEnabledOptimismL2Mock.new(this.bridge.address);
-    });
+  describe('Optimism', function () {
+    function encodeOptimismParams (params) {
+      return utils.defaultAbiCoder.encode(['bytes32', 'uint32'], [...Object.values(params)]);
+    }
 
-    it('should send cross-chain message successfuly', async function () {
+    describe('Optimism-L1', function () {
+      const bridgeId = '0x8a69005a3baed81c73049b861e928740f14876213fc1cafd636c7aa14c2576b1';
       const bridgeParameters = {
-        bridgeId: '0xa2b60698a3b22f7842b51a9f95163b4bfa9eb52f824e76e57efb81994621fb9b',
+        bridgeId,
         gasLimit: utils.parseEther('0.000000000001'),
       };
-      const crossChainTxParameters = utils.defaultAbiCoder.encode(
-        ['bytes32', 'uint32'],
-        [...Object.values(bridgeParameters)],
-      );
 
-      await this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters);
+      beforeEach(async function () {
+        this.bridge = await BridgeHelper.deploy('Optimism');
+        this.contract = await CrossChainEnabledOptimismL1Mock.new(this.bridge.address);
+      });
+
+      it('should send cross-chain message successfuly', async function () {
+        const crossChainTxParameters = encodeOptimismParams(bridgeParameters);
+
+        await this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters);
+      });
+
+      it('should restrict cross-chain calls with wrong bridge Id', async function () {
+        const wrongBridgeId = web3.utils.keccak256('wrong bridge id');
+
+        bridgeParameters.bridgeId = wrongBridgeId;
+
+        const crossChainTxParameters = encodeOptimismParams(bridgeParameters);
+
+        await expectRevertCustomError(
+          this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters),
+          `InvalidTargetBridge("${wrongBridgeId}", "${bridgeId}")`,
+        );
+      });
+
+      shouldBehaveLikeReceiver();
     });
 
-    shouldBehaveLikeReceiver();
+    describe('Optimism-L2', function () {
+      const bridgeId = '0xa2b60698a3b22f7842b51a9f95163b4bfa9eb52f824e76e57efb81994621fb9b';
+      const bridgeParameters = {
+        bridgeId,
+        gasLimit: utils.parseEther('0.000000000001'),
+      };
+
+      beforeEach(async function () {
+        this.bridge = await BridgeHelper.deploy('Optimism');
+        this.contract = await CrossChainEnabledOptimismL2Mock.new(this.bridge.address);
+      });
+
+      it('should send cross-chain message successfuly', async function () {
+        const crossChainTxParameters = encodeOptimismParams(bridgeParameters);
+
+        await this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters);
+      });
+
+      it('should restrict cross-chain calls with wrong bridge Id', async function () {
+        const wrongBridgeId = web3.utils.keccak256('wrong bridge id');
+
+        bridgeParameters.bridgeId = wrongBridgeId;
+
+        const crossChainTxParameters = encodeOptimismParams(bridgeParameters);
+
+        await expectRevertCustomError(
+          this.contract.sendCrossChainMessage(destination, calldata, crossChainTxParameters),
+          `InvalidTargetBridge("${wrongBridgeId}", "${bridgeId}")`,
+        );
+      });
+
+      shouldBehaveLikeReceiver();
+    });
   });
 
   describe('Polygon-Child', function () {
